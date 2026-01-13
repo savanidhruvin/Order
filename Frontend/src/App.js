929d4d74583e4f18f8027d74cb702c55e6130abe
savanidhruvin <dhruvinsavani9313@gmail.com>
2026-01-13 10:32:50 +0530

    first commit

---
+++ Frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Order from './Components/Order'
+
+const App = () => {
+  return (
+    <div>
+      <Order/>
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/Components/Order.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Order = () => {
+  return (
+    <div>
+       <div>
+          <h1>Order</h1>
+       </div>
+    </div>
+  )
+}
+
+export default Order
