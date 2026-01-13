a981b0485c030afe1da257ea6e0d7950e162e813
savanidhruvin <dhruvinsavani9313@gmail.com>
2026-01-13 16:40:50 +0530

    savani13-01

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': "320px",
+        'mb': "375px",
+        'ml': "425px",
+        'sx': "376px",
+        'ms': "576px",
+        'md600': "601px",
+        'md': "768px",
+        'mx':"426px",
+        'sm': "640px",
+        'dm': "992px",
+        'lg': "1024px",
+        'xl': "1280px",
+        "2xl": "1440px",  
+        "3xl": "1650px",
+        "4xl": "1920px",
+        "5xl": "2560px",
+      },
+    },
   },
   plugins: [],
 }
